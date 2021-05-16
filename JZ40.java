6d7bdf425d1ffdd9bfa43523b2b46c066a4179e3
yat <303980015@qq.com>
2021-05-16 23:47:16 +0800

    TopK，实现了partition方法，最大堆方法待实现

---
+++ JZ40.java
@@ -0,0 +1,54 @@
+/*
+ 题目描述
+ TopK
+ 输入整数数组 arr ，找出其中最小的 k 个数
+ 例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
+ */
+
+/*
+思路1：partition函数
+思路2：最大堆
+ */
+
+import java.util.Arrays;
+
+
+public class JZ40{
+}
+
+class JZ40Solution {
+    public int[] getLeastNumbers(int[] arr, int k) {
+        if (k==0 || arr.length==0){
+            return null;
+        }
+        return partitionArray(arr,0,arr.length-1,k-1);
+    }
+
+    public int[] partitionArray(int[] arr,int low,int high,int k){
+        int p = quickSort(arr,low,high);
+        if (p==k){
+            return Arrays.copyOf(arr,k+1);
+        }
+        return p>k?partitionArray(arr,low,p-1,k):partitionArray(arr,p+1,high,k);
+    }
+
+    public int quickSort(int[] arr,int left,int right){
+        int pivot = arr[left];
+        while (left < right){
+            while (left < right && pivot < arr[right]){
+                right--;
+            }
+            if (left < right){
+                arr[left++]=arr[right];
+            }
+            while (left < right && pivot > arr[left]){
+                left++;
+            }
+            if (left < right){
+                arr[right--] = arr[left];
+            }
+        }
+        arr[left] = pivot;
+        return left;
+    }
+}
